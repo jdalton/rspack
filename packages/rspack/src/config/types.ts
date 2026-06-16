49b831a673325b430bd5e78396abd1492fa8a4ae
AsyncIter <intellild@outlook.com>
2026-06-16 16:38:16 +0800

    feat: support import.meta.env and env merging

--- packages/rspack/src/config/types.ts
+++ packages/rspack/src/config/types.ts
@@ -4,6 +4,7 @@ import type { ChunkGraph } from '../ChunkGraph';
 import type { Compilation, PathData } from '../Compilation';
 import type { Compiler } from '../Compiler';
 import type { Chunk } from '../exports';
+import type { DotenvPluginOptions } from '../lib/DotenvPlugin';
 import type WebpackError from '../lib/WebpackError';
 import type { Module } from '../Module';
 import type ModuleGraph from '../ModuleGraph';
@@ -387,6 +388,11 @@ export type DevtoolModuleFilenameTemplate =
 /** A fallback is used when the template string or function above yields duplicates. */
 export type DevtoolFallbackModuleFilenameTemplate =
   DevtoolModuleFilenameTemplate;
+
+/**
+ * Enable and configure the Dotenv plugin to load environment variables from .env files.
+ */
+export type Dotenv = boolean | DotenvPluginOptions;
 
 /** Tell Rspack what kind of ES-features may be used in the generated runtime-code. */
 export type Environment = {
@@ -3225,6 +3231,10 @@ export type RspackOptions = {
    * The source map configuration.
    */
   devtool?: DevTool;
+  /**
+   * Enable and configure the Dotenv plugin to load environment variables from .env files.
+   */
+  dotenv?: Dotenv;
   /**
    * Options for Node.js environment.
    */
